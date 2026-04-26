8e891bda24101f927d1a938b832eb168c555236a
Alan Sereno <alanserenobraz@gmail.com>
2026-04-26 12:49:12 -0300

    corrige todo o folder pagamento para classes privadas com __ como o painted pediu :)

--- pagamentos/pagamento.py
+++ pagamentos/pagamento.py
@@ -4,9 +4,20 @@ from abc import ABC, abstractmethod
 # afinal ninguem paga na vida real com o mero conceito de pagamento, mas sim com cartao pix etc
 class Pagamento(ABC):
     def __init__(self, valor: float):
-        self.valor = valor
-        self.aprovado = False
+        self.__valor = valor
         #vamos comecar com aprovado como falso, e so depois de processar o pagamento que ele pode ser aprovado ou nao
+        self.__aprovado = False
+
+        #vamos precisar usar getters agora, porque a equipe escolheu tratar os atributos como privados (com __) para garantir o encapsulamento
+        #seguindo um excelente padrao de boas praticas:
+        
+        def get_valor(self) -> float:
+            return self.__valor
+        def get_aprovado(self) -> bool:
+            return self.__aprovado
+        #definiremos tambem um setter para o atributo aprovado, para que as subclasses possam atualizar esse status depois de processar o pagamento
+        def set_aprovado(self, status: bool):
+            self.__aprovado = status
         #o decorator @abstractmethod indica que o metodo processar deve ser implementado por qualquer classe que herde de Pagamento
     @abstractmethod
     def processar(self) -> bool:
--- pagamentos/pix.py
+++ pagamentos/pix.py
@@ -5,12 +5,17 @@ class Pix(Pagamento):
         #chama o construtor da classe base Pagamento para inicializar o valor.
         super().__init__(valor)
         #a chave_destino é um atributo específico para o Pix, que representa a chave Pix do destinatário do pagamento
-        self.chave_destino = chave_destino
+        self.__chave_destino = chave_destino
+        def get_chave_destino(self) -> str:
+            return self.__chave_destino
+        
     def processar(self) -> bool:
         #aqui implementamos a lógica de processamento do pagamento via Pix
         print(f"Iniciando transação Pix...")
-        print(f"Transferindo R$ {self.valor:.2f} para a chave: {self.chave_destino}")
+        print(f"Transferindo R$ {self.get_valor():.2f} para a chave: {self.__chave_destino}")
         #para simplificar, vamos assumir que o pagamento é sempre aprovado :)
-        self.aprovado = True
+        #agora usamos set_aprovado ao inves de acessar diretamente o atributo, para manter o encapsulamento e a consistencia com as outras classes de pagamento
+        self.set_aprovado(True)
         print("Pagamento via Pix aprovado!\n")
-        return self.aprovado
+        
+        return self.get_aprovado()
